834991def16e055120793bb7df175a502c7e271f
Laith Harb <harblaith77@gmail.com>
2020-02-16 07:37:56 -0500

    Final styling touch ups

--- sprint-1/src/components/VideoInfo/VideoInfo.js
+++ sprint-1/src/components/VideoInfo/VideoInfo.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import './VideoInfo.scss'
+import './VideoInfo.scss';
+import ViewsIcon from './Icon-views.svg';
+import LikesIcon from './Icon-likes.svg'
 
 class VideoInfo extends Component {
     render() {
         return (
             <div className="VideoInfo">
                 <div className="VideoInfo__container">
-                <div className="VideoInfo__info-container">
-                    <h3 className="VideoInfo__title">
-                        BMX Rampage: 2018 Highlights
-                    </h3>
-                    <h6 className="VideoInfo__author">
-                        By Red Cow
-                        <span className="VideoInfo__date">
-                            12/18/2018
-                        </span>
-                    </h6>
-                </div>
+                    <div className="VideoInfo__info-container">
+                        <h3 className="VideoInfo__title">
+                            BMX Rampage: 2018 Highlights
+                        </h3>
+                        <div className="VideoInfo__author-container">
+                            <h6 className="VideoInfo__author">
+                                By Red Cow
+                                <span className="VideoInfo__date">
+                                    12/18/2018
+                                </span>
+                            </h6>
+                            <div className="VideoInfo__stats-container">
+                                <span className="VideoInfo__stats">
+                                    <img src={LikesIcon} className="VideoInfo__stats-icon" alt=""/>
+                                    100,232
+                                </span>
+                                <span className="VideoInfo__stats">
+                                    <img src={ViewsIcon} className="VideoInfo__stats-icon" alt=""/>
+                                    1,102,322
+                                </span>
+                            </div>
+                            
+                        </div>
+                        
+                        
+                    </div>
                 <div className="VideoInfo__description-container">
                     <p className="VideoInfo__description">
                         On a gusty day in Southern Utah, a group of 25
